0381c8de8953c7d2f0397aceda093eb07a92f771
Kevin Miller <kevinwmiller@protonmail.com>
2018-12-02 12:25:22 -0500

    travis should now install protbuf before fetching dependencies

---
+++ vet.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+PROTOBUF_VERSION=3.3.0
+PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+
+curl -OL https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
+
+# Unzip
+unzip ${PROTOC_FILENAME} -d protoc3
+
+# Move protoc to /usr/local/bin/
+sudo mv protoc3/bin/* /usr/local/bin/
+
+# Move protoc3/include to /usr/local/include/
+sudo mv protoc3/include/* /usr/local/include/
+
+# Optional: change owner
+sudo chown $USER /usr/local/bin/protoc
+sudo chown -R $USER /usr/local/include/google
